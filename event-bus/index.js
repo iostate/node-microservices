9a8410274daf9d4e5a3d98a43246e47448c64061
Quan Truong <qmtruong92@gmail.com>
2023-07-01 12:26:09 -0700

    dockerize event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,30 +5,54 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+
+const events = [];
+
 const PORT = 4005;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post('/events', async (req, res) => {
+app.get('/events', (req, res) => {
+  res.status(200).send(events);
+});
+
+app.post('/events', (req, res) => {
   const event = req.body;
+  events.push(event);
 
-  await axios.post('http://localhost:4000/events', event).catch((err) => {
+  axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err);
   });
 
-  await axios.post('http://localhost:4001/events', event).catch((err) => {
+  axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err);
   });
-  await axios.post('http://localhost:4002/events', event).catch((err) => {
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err);
+  });
+  axios.post('http://localhost:4003/events', event).catch((err) => {
     console.log(err);
   });
 
-  res.status(201).send('Status: OK');
+  res.status(200).send('Status: OK');
 
   // axios.post('http://locahost:4003/events', event).catch(err) {
   //   console.log(err);
   // }
 });
 
-app.listen(PORT, console.log(`Listening on port ${PORT}`));
+app.listen(PORT, async () => {
+  console.log(`Listening on port ${PORT}`);
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+
+    for (let event of res.data) {
+      console.log('Processing event:', event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+});
