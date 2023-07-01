e4f05532f18724f8118daa1a29fd908342333fd6
Quan Truong <qmtruong92@gmail.com>
2023-07-01 12:25:54 -0700

    dockerize client

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,21 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const CommentList = ({ comments }) => {
-  // const [comments, setComments] = useState([]);
-
-  // const fetchData = async () => {
-  //   const res = await axios.get(
-  //     `http://localhost:4001/posts/${postId}/comments`
-  //   );
-
-  //   setComments(res.data);
-  // };
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
   const renderedComments = comments.map((comment) => {
+    let status = comment.status;
+    console.log(status);
+    if (status === 'pending') {
+      comment.content = 'This post is awaiting approval.';
+    }
+    if (status === 'rejected') {
+      comment.content = 'This post has been rejected';
+    }
+
     return <li key={comment.id}>{comment.content}</li>;
   });
 
